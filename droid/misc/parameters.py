59800d59cf4e7d128a1baa1ea948220a0ed78498
robot <romerhomerobotics@gmail.com>
2025-12-26 04:51:41 +0300

    parameters

--- droid/misc/parameters.py
+++ droid/misc/parameters.py
@@ -2,12 +2,12 @@ import os
 from cv2 import aruco
 
 # Robot Params #
-nuc_ip = ""
-robot_ip = ""
-laptop_ip = ""
-sudo_password = ""
-robot_type = ""  # 'panda' or 'fr3'
-robot_serial_number = ""
+nuc_ip = "10.0.0.3"
+robot_ip = "10.0.0.2"
+laptop_ip = "10.0.0.1"
+sudo_password = "kovan123"
+robot_type = "panda"  # 'panda' or 'fr3'
+robot_serial_number = "295341-1324423"
 
 # Camera ID's #
 hand_camera_id = ""
@@ -22,7 +22,7 @@ CHARUCOBOARD_MARKER_SIZE = 0.016
 ARUCO_DICT = aruco.Dictionary_get(aruco.DICT_5X5_100)
 
 # Ubuntu Pro Token (RT PATCH) #
-ubuntu_pro_token = ""
+ubuntu_pro_token = "C14KZhAZx5JVyqCXnu6D7Ahy5cCGRh"
 
 # Code Version [DONT CHANGE] #
 droid_version = "1.3"
